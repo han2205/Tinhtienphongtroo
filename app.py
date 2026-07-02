2bd8f7227773b4924a8e4a1e2734c4f3f3e62058
han2205 <23030122@student.bdu.edu.vn>
2026-07-02 13:52:55 +0700

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.set_page_config(page_title="Tính Tiền Phòng Trọ", page_icon="🏠")
 
 st.image("logo.jpg", use_container_width=True)
 
-st.title("🏠 Tính tiền phòng trọ - Nhóm 2")
+st.title("🏠 Tính tiền phòng trọ - ĐỀ TÀI 1")
 
 # Nhập tiền phòng
 A = st.number_input("Nhập số tiền phòng (đồng)", min_value=0.0, value=2000000.0)
